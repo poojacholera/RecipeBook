ab2c4e78aa1eb66b0bf93799ec5149a8ebe52d60
poojacholera <poojacholera1@gmail.com>
2020-05-10 22:32:11 -0400

    country select option included in CoronaInfo | cases(types) for each country can be viewed | data Source name added

--- src/CoronaInfo.jsx
+++ src/CoronaInfo.jsx
@@ -1,67 +1,136 @@
 import React, {useEffect, useState} from "react";
-import styles from "./CSS/CoronaInfo.module.css"
-import CountDisplayContainer from './CountDisplayContainer';
+import styles from "./CSS/CoronaInfo.module.css";
+import CountDisplayContainer from "./CountDisplayContainer";
 import LineChartContainer from "./LineChartContainer";
-import Chart from 'chart.js';
-const CoronaInfo = () => {
+import Select from '@material-ui/core/Select';
 
+
+const CoronaInfo = () => {
     const [globalData, setGlobalData] = useState([]);
     const [countriesData, setCountriesData] = useState([]);
     const [dayOneData, setDayOneData] = useState([]);
+    const [provinceList, setProvinceList] = useState([]);
+    const [countryList,setCountryList] =useState([]);
+    const [selectCountry, setSelectCountry] = React.useState({});
 
+    /*const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);*/
 
     const API_summary = `https://api.covid19api.com/summary`;
-    const API_DayOne = `https://api.covid19api.com/dayone/country/canada/status/confirmed`;
+    const API_countryList = `https://api.covid19api.com/countries`;
+    var API_DayOne = `https://api.covid19api.com/dayone/country/canada/status/confirmed`;
 
     useEffect(() => {
-     //  getTotalGlobalCount();
-        getCountByDayOne();
-    }, []);
+        console.log("1 - useEffect ran... ");
+        getTotalGlobalCount();
+         getCountryList();
+    },[]);
 
-    const getTotalGlobalCount = async () =>{
-        const response = await fetch(API_summary);
-        const data = await response.json();
-        setGlobalData(data.Global);
-        setCountriesData(data.Countries);
+    const getTotalGlobalCount = async () => {
+        await fetch(API_summary)
+            .then(res => res.json())
+            .then(data => {
+                setGlobalData(data.Global);
+                setCountriesData(data.Countries);
+            });
     };
-    const getCountByDayOne = async () =>{
-        const response = await fetch(API_DayOne);
-        const data = await response.json();
-        setDayOneData(data);
-        console.log("DayOne data: "+ data[1].Country);
-        console.log("DayOne data: "+ data[1].Province);
-       // getProvinceList(data)
+    const getCountryList = async() => {
+      await fetch(API_countryList)
+            .then(res => res.json())
+            .then(data => {
+                setCountryList(data);
+            });
     };
-/*const getProvinceList = (data) => {
-   // const unique = [...new Set(array.map(item => item.age))];
-    const uniqueProvinces = [...new Set(data.map(item => item.Province))];
-};*/
+    const handleCountryChange = (event) => {
+        onCountrySelect(event.target.value).then((res) => {
+            setSelectCountry(res[0]);
+            getCountByDayOne(res[0].Slug);
+        });
+    };
+    const onCountrySelect = async (c) => {
+        const selectedObj = countriesData.filter((i)=>{
+            return i.CountryCode ===  c ;
+        });
+        console.log(selectedObj);
+        return selectedObj;
+    }
 
-
+    const getCountByDayOne = async (val) => {
+        //fetchData()
+        //TODO error check for val
+        API_DayOne = `https://api.covid19api.com/dayone/country/${val}`;
+        console.log(API_DayOne)
+        let data = await fetch(API_DayOne)
+            .then(res => res.json());
+        setDayOneData([...dayOneData, data]);
+        console.log(data);
+        // if data is empty ELSE if province is empty
+        if(JSON.stringify(data) === '[]' ){
+            console.log("province emtpty")
+            //  TODO
+            //console.log(data);
+        }else {
+            if(!(data[0].Province === "")){
+                const list = getProvinceList(data);
+                setProvinceList(list);
+            }
+        }
+    };
+    /**
+     * get List of names of province
+     */
+    const getProvinceList = (data) => {
+        let unique = [...new Set(data.map(item => item.Province))];
+        return unique;
+    };
+    /*const getProvinceList = (data) => {
+     // const unique = [...new Set(array.map(item => item.age))];
+      const uniqueProvinces = [...new Set(data.map(item => item.Province))];
+  };*/
+    const countryOptions = countryList.map(obj =>
+            <option value={obj.ISO2} label={obj.Country} >{obj.Country}</option>
+        )
     return (
-          <div className={styles.newsInfo} id="newsInfo">
-              <h1 className="text-white-50 align-content-center justify-content-around font-weight-bolder">News Book</h1>
-              <CountDisplayContainer
-                  heading={"Total"}
-                  data0={globalData.TotalConfirmed}
-                  data1={globalData.TotalRecovered}
-                  data2={globalData.TotalDeaths}
-                  labels={["Cases","Recovered","Death"]}
-              />
-              <hr style={{width: '80%'}}/>
-              <CountDisplayContainer
-                  heading={"Today Updates"}
-                  data0={globalData.NewConfirmed}
-                  data1={globalData.NewRecovered}
-                  data2={globalData.NewDeaths}
-                  labels={["New Cases"," New Recovered","New Death"]}
-              />
+        <div className={styles.newsInfo} id="newsInfo" onLoad={() => getTotalGlobalCount()}>
+            <h3 className="text-white-50 align-content-center justify-content-around font-weight-bolder">
+                News Book
+            </h3>
 
-              <LineChartContainer data={dayOneData}/>
-          </div>
+            <h1><b>C<i className="fas fa-virus fa-x" style={{color:"darkcyan"}}></i>VID-19</b></h1>
+
+            <CountDisplayContainer
+                heading={"Total"}
+                data0={globalData.TotalConfirmed}
+                data1={globalData.TotalRecovered}
+                data2={globalData.TotalDeaths}
+                labels={["Cases", "Recovered", "Death"]}
+            />
+            <hr style={{width: "80%"}}/>
+
+            <div >
+                <label id="demo-simple-select-label">Select Country</label>
+                <Select
+                    id="demo-simple-select"
+                    value={selectCountry.Country}
+                    onChange={(val)=>handleCountryChange(val)}
+                >
+                    {countryOptions}
+
+                </Select>
+
+                {!(JSON.stringify(selectCountry) === '{}') &&
+                    <CountDisplayContainer
+                    heading={`${selectCountry.Country}`}
+                    data0={selectCountry.TotalConfirmed}
+                    data1={selectCountry.TotalRecovered}
+                    data2={selectCountry.TotalDeaths}
+                    labels={["Cases", "Recovered", "Death"]}
+                    />
+                }
+                <LineChartContainer data={dayOneData} provinceNames={provinceList}/>
+            </div>
+            <p>Source: https://www.covid19api.com/</p>
+        </div>
     );
 };
 export default CoronaInfo;
-
-
-
--- src/LineChartContainer.jsx
+++ src/LineChartContainer.jsx
@@ -1,131 +1,203 @@
 import React, {useEffect, useState} from "react";
-import styles from "./CSS/CoronaInfo.module.css"
-import Chart from 'chart.js';
+import styles from "./CSS/CoronaInfo.module.css";
+import Chart from "chart.js";
 import {map} from "react-bootstrap/cjs/ElementChildren";
+import {ListGroup} from "react-bootstrap";
 
-const LineChartContainer = (props) => {
+const LineChartContainer = props => {
     //  console.log("props : "+ props.data);
 
     // var Jan = 0, Feb = 0, Mar = 0, Apr = 0,May = 0, Jun =0, Jul = 0, Aug = 0, Sept=0, Oct =0 , Nov=0, Dec=0;
     //const [status, setStatus] = useState('');
-    const [lastDate, setLastDate] = useState('');
-    const [province, setProvince] = useState('');
+    const [lastDate, setLastDate] = useState("");
+    const [province, setProvince] = useState("");
+    const [provinceNames, setProvinceNames] = useState([])
     const [casesData, setCasesData] = useState([]);
+    const [provinceData, setProvinceData] = useState([]);
     const chartRef = React.createRef();
 
     useEffect(() => {
-        //let li = getOntarioProvince();
-        let li = getProvinceData();
-        // getQuebecProvince();
-        console.log('result: ' + li);
-        // getAxis();
-          loadLineChart(li);
-    });
-    const getProvinceData = () => {
-        let li = getOntarioProvince();
-        const liGroup = groupBy(li,"province");
-      console.log(liGroup);
-        const formatedLi = liGroup.map((i,index) => {
-            console.log("li[index] : " + liGroup[index]["province"]);
-           /* let cases = parseInt(i.cases, 10);
-            // console.log("cases : "+cases);
-            var name = 'province';
-            this[name+'_'+i.province] = [];
+       getProvinceData(props.provinceNames);
+        // loadLineChart(li);
+       // console.log(props.data);
+        console.log("2 - useEffect ran... ");
+    }, [props]);
+
+    const getProvinceData =  names => {
+        console.log(names);
+        console.log("getProvinceData()");
+        const li =  getOntarioProvince();
+
+        // console.log(names);
+        for (let i = 0; i < names.length; ++i) {
+            console.log(names[i]);
+            var result = li.filter(e => {
+                return names[i] === e.province;
+            });
+//TODO use accumulator here for array of objects in which get the list of cases
+            console.log(result);
+            setProvinceData([
+                ...provinceData,
+                result
+            ]);
+        }
+
+        console.log(JSON.stringify(provinceData));
+        //console.log(JSON.stringify(li));
+
+
+        // const formatedLi = liGroup.map((i, index) => {
+        //  console.log("li[index] : " + liGroup[index]["province"]);
+        //const innerData = li[index];
+        // setCasesData([]);
+        //console.log("innerData : " + innerData);
+        //console.log("Data : " + innerData.cases + " - " + innerData.province);
+        /*let cases = parseInt(j.cases, 10);
+            console.log("cases : " + cases);
             setCasesData(casesData => [...casesData, cases]);
-            console.log(casesData);*/
+            console.log(casesData);
+            p=j.province;
 
-
-        });
-        return formatedLi;
+          });*/
+        /* setProvinceData([
+            ...provinceData,
+            {
+              cases: casesData,
+              province: province
+            }
+          ]);*/
+        //   console.log("provinceDAta: " + provinceData);
+        //  });
+        //return formatedLi;
     };
 
-
-    const getOntarioProvince = () => {
-        setCasesData([]);
-        const li = props.data/*.slice(0,1)*/
-            .filter(i => (i.Province === 'Ontario' || i.Province === 'Quebec' ))
+    const getOntarioProvince =  () => {
+        console.log("getOntarioProvince()");
+      //  console.log((JSON.stringify(props.data[0])));
+        console.log(props.data);
+        const li = props.data.filter(i => { return i.Province === "Ontario" || i.Province === "Quebec"})
             .filter(i => {
                 //gets today's date and month
+
                 const today = new Date();
-                let currentMonth =parseInt(today.getMonth(),10) +1;
-                let currentDate =parseInt(today.getDate(),10)-1 ;
+                let currentMonth = parseInt(today.getMonth(), 10) + 1;
+                let currentDate = parseInt(today.getDate(), 10) - 1;
                 //gets ith date's month and date
-                const dateFormat = new Date(i['Date']);
-                let dateFormatMonth =parseInt(dateFormat.getMonth(),10) +1;
-                let dateFormatDate =parseInt(dateFormat.getDate(),10) +1;
+                const dateFormat = new Date(i["Date"]);
+                let dateFormatMonth = parseInt(dateFormat.getMonth(), 10) + 1;
+                let dateFormatDate = parseInt(dateFormat.getDate(), 10) + 1;
                 //gets last day of the month
-                let dt = new Date(dateFormat.getFullYear(), dateFormat.getMonth() +1, 0);
-                let dtMonth =parseInt(dt.getMonth(),10) +1;
-                let dtDate =parseInt(dt.getDate(),10) +1;
+                let dt = new Date(
+                    dateFormat.getFullYear(),
+                    dateFormat.getMonth() + 1,
+                    0
+                );
+                let dtMonth = parseInt(dt.getMonth(), 10) + 1;
+                let dtDate = parseInt(dt.getDate(), 10) + 1;
                 //console.log("date "+ dateFormatDate+"-"+dateFormatMonth +" -X- "+ dtDate+"-"+dtMonth);
                 //console.log("date "+ dateFormatDate+"-"+dateFormatMonth +" -X- "+ currentDate+"-"+currentMonth);
                 // check if the ith date is last date of the month or if it is the current month's date which is before the last day of month
-                return (dateFormatMonth == dtMonth && dateFormatDate == dtDate) || (dateFormatMonth == currentMonth && dateFormatDate == currentDate);
+                return (
+                    (dateFormatMonth == dtMonth && dateFormatDate == dtDate) ||
+                    (dateFormatMonth == currentMonth && dateFormatDate == currentDate)
+                );
             })
             .map(function (i) {
-              //  console.log("cases " + i.Cases+" on date " +i['Date']);
+                //  console.log("cases " + i.Cases+" on date " +i['Date']);
                 setProvince(i.Province);
-                let month = i['Date'].substr(5, 2);
-                let date = new Date(i['Date']);
-                console.log("date:"+date);
+                let month = i["Date"].substr(5, 2);
+                let date = new Date(i["Date"]);
+                 console.log("date:" + date);
                 let cases = parseInt(i.Cases, 10);
-              // setCasesData(casesData =>casesData.concat(cases));
+                 setCasesData(casesData =>casesData.concat(cases));
 
-                setLastDate(i['Date']);
+                setLastDate(i["Date"]);
                 return {
                     country: i.Country,
                     province: i.Province,
                     date: date,
                     cases: cases,
-                    lastDate: lastDate,
+                    lastDate: lastDate
                 };
             });
-//TODO sorting by month (safe-side)
+        //TODO sorting by month (safe-side)
 
-/*const p = li.length;
-    for(k =0; k<=li.length; ++k){
-
-    }*/const today = new Date();
-        let dt = new Date(today.getFullYear(), today.getMonth() +1, -1);
+        const today = new Date();
+        let dt = new Date(today.getFullYear(), today.getMonth() + 1, -1);
         /*const lastdt = new Date(lastDate).toString();
-        setLastDate(lastdt);    */
+            setLastDate(lastdt);    */
+        console.log(li);
         return li;
     };
 
-
-
-    const loadLineChart = (li) => {
-           console.log(li);
+    const loadLineChart = li => {
+        //  console.log(li);
         var ctx = chartRef.current.getContext("2d");
         new Chart(ctx, {
-            type: 'line',
+            type: "line",
             data: {
                 labels: ["Jan", "Feb", "Mar", "Apr"],
-                datasets: li,
+                datasets: [3, 15, 1048, 6648]
             },
             options: {
                 responsive: true,
                 scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: true
+                            }
                         }
-                    }]
+                    ]
                 }
             }
         });
     };
     return (
         <div>
-            <h5><span style={{color: "darkcyan"}}>Last updated on:</span>{lastDate}</h5>
-            <canvas id={"myChart"} ref={chartRef} style={{width: "100vh", height: "80vh"}}></canvas>
+            <h5>
+                <span style={{color: "darkcyan"}}>Last updated on:</span>
+                {lastDate}
+            </h5>
+            <canvas
+                id={"myChart"}
+                ref={chartRef}
+                style={{width: "100vh", height: "80vh"}}
+            />
         </div>
     );
 };
 export default LineChartContainer;
 
-function groupBy(arr, prop) {
+function groupBy1(arr, prop) {
     const map = new Map(Array.from(arr, obj => [obj[prop], []]));
     arr.forEach(obj => map.get(obj[prop]).push(obj));
     return Array.from(map.values());
 }
+
+function groupByFields(xs, groupFields) {
+    groupFields = [].concat(groupFields);
+    return xs.reduce(function (rv, x) {
+        let groupKey = groupFields.reduce((keyObject, field) => {
+            keyObject[field] = x[field];
+            return keyObject;
+        }, {});
+        (rv[JSON.stringify(groupKey)] = rv[JSON.stringify(groupKey)] || []).push(x);
+        return rv;
+    }, {});
+}
+
+const groupBy2 = (data, key) => {
+    return data.reduce(function (acc, item) {
+        (acc[item[key]] = acc[item[key]] || []).push(item);
+        return acc;
+    }, {});
+};
+var groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
+//console.log(groupBy(['one', 'two', 'three'], 'length'));
+// => {3: ["one", "two"], 5: ["three"]}
