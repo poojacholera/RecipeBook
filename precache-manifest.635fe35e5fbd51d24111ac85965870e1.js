4e20da4145e708519b026bccc6e7cd615221a96d
poojacholera <poojacholera1@gmail.com>
2020-05-19 19:58:09 -0400

    Updates

---
+++ precache-manifest.635fe35e5fbd51d24111ac85965870e1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "249fb2842671e312b3695753c5447dd0",
+    "url": "/RecipeBook/index.html"
+  },
+  {
+    "revision": "425cc39016cde4865839",
+    "url": "/RecipeBook/static/css/2.3a3c2869.chunk.css"
+  },
+  {
+    "revision": "1b26d14e9b554bf1e8cc",
+    "url": "/RecipeBook/static/css/main.d6b1e189.chunk.css"
+  },
+  {
+    "revision": "425cc39016cde4865839",
+    "url": "/RecipeBook/static/js/2.7d0b4ec1.chunk.js"
+  },
+  {
+    "revision": "1b26d14e9b554bf1e8cc",
+    "url": "/RecipeBook/static/js/main.9ac64956.chunk.js"
+  },
+  {
+    "revision": "465103f1f39e4ff50632",
+    "url": "/RecipeBook/static/js/runtime~main.3c89a3ed.js"
+  },
+  {
+    "revision": "6fb2f079a3f6de256592b0f5198daaad",
+    "url": "/RecipeBook/static/media/Halfbred-ALLz6.6fb2f079.otf"
+  },
+  {
+    "revision": "d0d3bb8552576d8abd799a5d10d2d901",
+    "url": "/RecipeBook/static/media/Halfbred-jEEq0.d0d3bb85.ttf"
+  },
+  {
+    "revision": "04146a9daa28817096b52f9f7967e4c5",
+    "url": "/RecipeBook/static/media/HemeraIiDemoRegular-ZY73.04146a9d.ttf"
+  },
+  {
+    "revision": "f30a0bc95de7ef8f063f00fbc0a1512f",
+    "url": "/RecipeBook/static/media/Raphtalia-2ZmK.f30a0bc9.ttf"
+  },
+  {
+    "revision": "8f995c6ada3a5b879ef9581331e8825c",
+    "url": "/RecipeBook/static/media/SkinnyThings-G1dq.8f995c6a.ttf"
+  },
+  {
+    "revision": "6e39b2b7f38cb07c74921f8899a744f9",
+    "url": "/RecipeBook/static/media/SkinnyThingsItalic-gnLR.6e39b2b7.ttf"
+  },
+  {
+    "revision": "b623d62a31c20152196b98ade7460a63",
+    "url": "/RecipeBook/static/media/ZilapMagic-vmnRM.b623d62a.ttf"
+  }
+]);
